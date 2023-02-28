7c7b2b6795857c160d175d974a10d52579205a95
SRAMIREZ <sramirez@financierafama.com.ni>
2023-02-27 20:41:45 -0600

    first commit

---
+++ BlazorApp1/Shared/Models/UriFama.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.Models
+{
+    public class UriFama
+    {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+        public bool State { get; set; }
+        public DateTime LastComprobation { get; set; }
+    }
+}
